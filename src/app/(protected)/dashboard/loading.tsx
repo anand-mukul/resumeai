22476ecb1226a932fc8bd3e5b2ed809a377fca94
Mukul Anand <mukulanand.biz@gmail.com>
2024-12-21 21:06:13 +0530

    Update Loading component color for improved visual feedback during loading state

--- src/app/(protected)/dashboard/loading.tsx
+++ src/app/(protected)/dashboard/loading.tsx
@@ -4,7 +4,7 @@ import React from "react";
 const Loading = () => {
   return (
     <div className="flex h-screen w-full items-center justify-center">
-      <Loader className="text-2xl" color="#6366f1" state>
+      <Loader className="text-2xl" color="#fc67fa" state>
         ...Loading Resumes
       </Loader>
     </div>
